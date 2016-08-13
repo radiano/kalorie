4bb2a66c5c66f9b3138596988e03bb6ec75de039
radiano <r.danis88@gmail.com>
2016-08-13 13:05:52 +0200

    Kalorie

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace products
+{
+    public partial class MainWindow : Window
+    {
+
+        Produkt Produkty = new Produkt();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Produkty.Create();
+            ShowItemsInTable();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Tabelka.Items.Clear();
+            ShowItemsInTable(IloscKalorii.Text, grupa.Text);
+        }
+
+        private void ShowItemsInTable(string kalorie = null, string grupa = null)
+        {
+
+            if (kalorie == null && grupa == null)
+            {
+                foreach (var item in Produkty.getProd())
+                {
+                    Tabelka.Items.Add(item);
+                }
+            }
+            else
+            {
+                int k;
+                bool boolKalorie = int.TryParse(kalorie, out k);
+
+                if (boolKalorie)
+                {
+                    foreach (var item in Produkty.getProd())
+                    {
+                        if (grupa == item.grupa)
+                        {
+                            float ka = 20 / item.kalorie;
+                            item.waga = k / item.kalorie;
+                            Tabelka.Items.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Podaj prawidłową wartość kalorii");
+                }
+            }
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Kalorie2.TekstWindow TextWindow = new Kalorie2.TekstWindow();
+            //TextWindow.Show();
+        }
+    }
+}
---
+++ Produkt.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace products
+{
+    class Produkt
+    {
+        List<Prod> produkty = new List<Prod>();
+        private const string FILE_NAME = "produkty.data";
+        private string[] tabProduct;
+        private bool istniejePlik;
+
+        public void Create()
+        {
+            _getFile();
+
+            if(istniejePlik == true) { 
+                foreach (var prod in tabProduct)
+                {
+                    string[] split = prod.Split("|".ToCharArray());
+
+                    int k = Int32.Parse(split[1]);
+
+                    produkty.Add(new Prod() { nazwa = split[0], kalorie = k, grupa = split[2], polecany = split[3] });
+                }
+            }
+        }
+        public List<Prod> getProd()
+        {
+            return produkty;
+        }
+        private void _getFile()
+        {
+
+            try { 
+                tabProduct = File.ReadAllLines(FILE_NAME);
+                istniejePlik = true;
+            }
+            catch (Exception)
+            {
+                istniejePlik = false;
+                MessageBox.Show("Brak pliku z produktami");
+            }
+        }
+    }
+    class Prod
+    {
+        public string nazwa { get; set; }
+        public int kalorie { get; set; }
+        public float waga { get; set; }
+        public string grupa { get; set; }
+        public string polecany { get; set; }
+    }
+}
